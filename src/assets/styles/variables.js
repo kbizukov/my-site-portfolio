fd4d4df4ad47affd0bce89124e211f9764043f6c
Kirill Bizukov <kirillbizukov@gmail.com>
2018-06-11 14:20:41 +0300

    add blog page

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -3,5 +3,6 @@ module.exports = {
   "text-color": "#474b57",
   "light-blue": "#0548be",
   "triangle-beige": "#f2f0e8",
-  "triangle-brown": "#eae8e2"
+  "triangle-brown": "#eae8e2",
+  "triangle-blog": "#faf8f0"
 };
